ed72104c7e8a0ce0c88ef6f1ee6681b7a7eecf24
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-11-09 07:29:15 +0100

    raname

---
+++ ProducvitityTools.NetworkUtilities.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ProducvitityTools.NetworkUtilities.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = ExternalIP.Get();
+        }
+    }
+}
---
+++ ProducvitityTools.NetworkUtilities/ExternalIP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+
+namespace ProducvitityTools.NetworkUtilities
+{
+    public static class ExternalIP
+    {
+        public static string Get()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://ifconfig.me/ip");
+            return "ss";
+            //request.UserAgent = "curl"; // this will tell the server to return the information as if the request was made by the linux "curl" command
+
+            //string publicIPAddress;
+
+            //request.Method = "GET";
+            //using (WebResponse response = request.GetResponse())
+            //{
+            //    using (var reader = new StreamReader(response.GetResponseStream()))
+            //    {
+            //        publicIPAddress = reader.ReadToEnd();
+            //    }
+            //}
+
+            //return publicIPAddress.Replace("\n", "");
+        }
+    }
+}
